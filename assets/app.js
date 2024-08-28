d2cd6b9062e564075fbfbd8d8f7a69159b902575
Elias Chaaya <E.chaaya@elbaridgroup.local>
2024-08-28 08:57:49 +0300

    design

--- assets/app.js
+++ assets/app.js
@@ -7,5 +7,9 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
+
 
 
