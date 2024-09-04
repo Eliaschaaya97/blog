0e8e399cb62ce5afab9e076b1829c7805c807a2b
Najm <n.choueiry@elbaridgroup.local>
2024-09-04 14:30:53 +0300

    changing entities

--- src/Entity/Blog/Authors.php
+++ src/Entity/Blog/Authors.php
@@ -6,7 +6,7 @@ use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity
- * @ORM\Table(name="authors")
+ * @ORM\Table(name="blogauthors")
  */
 class Authors
 {
--- src/Entity/Blog/Categories.php
+++ src/Entity/Blog/Categories.php
@@ -6,7 +6,7 @@ use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity
- * @ORM\Table(name="categories")
+ * @ORM\Table(name="blogcategories")
  */
 class Categories
 {
